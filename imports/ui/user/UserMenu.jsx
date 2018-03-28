2f8492786f311a671a22d1004e7344e4a3cec532
daviddlaiton <ad.laiton10@uniandes.edu.co>
2018-03-28 18:46:38 -0500

    className's fixed

--- imports/ui/user/UserMenu.jsx
+++ imports/ui/user/UserMenu.jsx
@@ -15,7 +15,7 @@ export class UserMenu extends React.Component {
                 <div className="container-fluid" >
                     <h1>Welcome! </h1>
                     <br />
-                    <div className="row">
+                    <div className="row filterSelection">
                         <div className="col-sm-4">
                             <h2 className="filterSelection">Filter your selection:</h2>
                             <br />
@@ -23,16 +23,16 @@ export class UserMenu extends React.Component {
                                 <div className="col-sm-1"></div>
                                 <div className="col-sm-10">
                                     <h5>Choose the national team:</h5>
-                                    <div class="dropdown">
-                                        <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">National teams <span class="caret"></span></button>
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-header">Colombia</li>
+                                    <div className="dropdown">
+                                        <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">National teams <span className="caret"></span></button>
+                                        <ul className="dropdown-menu">
+                                            <li className="dropdown-header">Colombia</li>
                                             <li><a href="#">James "Tatareto" Rodriguez</a></li>
                                             <li><a href="#">Falcao "Rodilla frágil" García</a></li>
                                             <li><a href="#">David "La Araña" Ospina</a></li>
                                             <li><a href="#">Mohamed Amine Ben Amor </a></li>
-                                            <li class="divider"></li>
-                                            <li class="dropdown-header">Russia</li>
+                                            <li className="divider"></li>
+                                            <li className="dropdown-header">Russia</li>
                                             <li><a href="#">Putin</a></li>
                                         </ul>
                                     </div>
@@ -43,9 +43,9 @@ export class UserMenu extends React.Component {
                                 <div className="col-sm-1"></div>
                                 <div className="col-sm-10">
                                     <h5>Or Choose the number:</h5>
-                                    <div class="dropdown">
-                                        <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Number of the Sticker <span class="caret"></span></button>
-                                        <ul class="dropdown-menu">
+                                    <div className="dropdown">
+                                        <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Number of the Sticker <span className="caret"></span></button>
+                                        <ul className="dropdown-menu">
                                             <li><a href="#">1</a></li>
                                             <li><a href="#">2</a></li>
                                             <li><a href="#">3</a></li>
