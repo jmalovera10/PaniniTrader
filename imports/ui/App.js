d2c90ef7308542da63c6da5e20709ab242a7fac4
juanmanuel <jm.dominguez@uniandes.edu.co>
2018-03-29 13:49:54 -0500

    /menu route is now private

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,14 +1,36 @@
+import {Meteor} from "meteor/meteor";
 import React from "react";
 import {Home} from "./home/Home.js"
 import {UserMenu} from "./user/UserMenu.jsx"
-import {Link, Route, Switch} from "react-router-dom";
+import {Link, Route, Switch, Redirect} from "react-router-dom";
 import {BrowserRouter} from "react-router-dom";
+
 
 export const App = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}/>
-            <Route path="/menu" component={UserMenu}/>
+            <PrivateRoute authed={isAuth()} path="/menu" component={UserMenu}/>
         </Switch>
     </BrowserRouter>
 );
+//Checks if the user is authneticated.
+function isAuth(){
+    let userId = Accounts.userId();
+    if(userId){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+//Renders the private route
+const PrivateRoute = ({component: Component, authed, ...rest}) => {
+    return (
+      <Route
+        {...rest}
+        render={(props) => authed === true
+          ? <Component {...props} />
+          : <Redirect to={{pathname: '/', state: {from: props.location}}} />} />
+    )
+  }
